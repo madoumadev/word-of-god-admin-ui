85361c9e06363c4714470d324a3e4be10e6a2703
madoumadev <almega95@gmail.com>
2023-09-13 00:03:09 +0300

    Трансляция

--- src/services/client/streaming/components/ClientStreamingAsideNav.vue
+++ src/services/client/streaming/components/ClientStreamingAsideNav.vue
@@ -2,10 +2,11 @@
 import { defineComponent } from 'vue'
 import { mapGetters } from 'vuex'
 import AsideNavLayout from '@/components/shared/AsideNavLayout.vue'
+import HeroIcon from '@/components/icons/HeroIcon.vue'
 
 export default defineComponent({
   name: 'ClientStreamingAsideNav',
-  components: { AsideNavLayout },
+  components: { HeroIcon, AsideNavLayout },
 
   computed: {
     ...mapGetters({
@@ -17,10 +18,40 @@ export default defineComponent({
 
 <template>
   <AsideNavLayout>
-    <div class="mt-4 flex flex-col">
-      <ul>
-        Streaming
-      </ul>
+    <div class="mt-4 flex flex-col p-6 overflow-hidden space-y-10 divide-y h-full">
+      <div class="flex flex-col">
+        <p class="capitalize text-gray-400 text-sm mb-4">БЛИЖАЙШАЯ ТРАНСЛЯЦИЯ</p>
+        <div class="inline-flex items-center mb-2 space-x-2">
+          <HeroIcon icon-type="outline" icon-name="CalendarIcon" class="w-4 h-5 text-gray-400" />
+          <p class="text-sm text-black capitalize font-bold">13 СЕНТЯБРЯ, СРЕДА</p>
+        </div>
+        <div class="inline-flex items-center space-x-2">
+          <HeroIcon icon-type="outline" icon-name="ClockIcon" class="w-4 h-5 text-gray-400" />
+          <p class="text-sm font-bold capitalize text-black">В 19:00</p>
+        </div>
+      </div>
+      <div class="flex flex-col space-y-2 pt-4">
+        <p class="uppercase text-gray-400 text-sm mb-4">Адрес</p>
+        <div class="">
+          <p class="text-sm text-black capitalize font-bold">
+            ЦЕРКОВЬ ЕВАНГЕЛЬСКИХ ХРИСТИАН "БЛАГАЯ ВЕСТЬ", Г. МОСКВА
+          </p>
+        </div>
+      </div>
+      <div class="flex flex-col space-y-2 pt-4">
+        <p class="uppercase text-gray-400 text-sm mb-4">СЛУЖЕНИЯ</p>
+        <ul class="list-disc pl-4">
+          <li class="text-sm text-black capitalize font-bold mb-2">ВОСКРЕСЕНЬЕ - 12:00,</li>
+          <li class="text-sm text-black capitalize font-bold">СРЕДА - 19:00</li>
+        </ul>
+      </div>
+
+      <div class="flex flex-col space-y-2 pt-4">
+        <p class="uppercase text-gray-400 text-sm mb-4">КОНТАКТЫ</p>
+        <div class="">
+          <p class="text-sm text-black font-bold">info@wordofgod.ru</p>
+        </div>
+      </div>
     </div>
   </AsideNavLayout>
 </template>
--- src/services/client/streaming/views/StreamingClient.vue
+++ src/services/client/streaming/views/StreamingClient.vue
@@ -1,6 +1,44 @@
 <template>
-  <div class="overflow-y-auto h-full">
-    <div>streaming page</div>
+  <div class="overflow-y-auto h-full p-4 sm:p-0">
+    <div class=" ">
+      <h5 class="text-2xl text-gray-900 pb-10">Трансляция</h5>
+    </div>
+    <div class="max-w-screen-md mx-auto">
+      <figure class="my-10 text-center">
+        <svg
+          class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 14"
+        >
+          <path
+            d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
+          />
+        </svg>
+        <blockquote>
+          <p class="text-md italic text-gray-500">
+            Если у вас нет церкви, куда пойти, мы будем очень рады, если вы присоединитесь к
+            трансляции нашего Богослужения. Но, если у вас есть пастор и церковь, которую вы
+            поддерживаете, тогда посещайте вашу драгоценную церковь. Стойте с вашим пастором,
+            помогайте ему всем, чем только можете. Поддерживайте его духовно. Поститесь и молитесь
+            за него. Имейте доверие к нему, и Бог поведёт вашу церковь к славному Дню, прихода
+            которого мы все ожидаем.
+          </p>
+        </blockquote>
+      </figure>
+
+      <div class="grid sm:grid-cols-2 grid-cols-1 gap-4 sm:gap-10">
+        <div class="bg-white p-6 rounded-3xl">
+          <button type="button" class="wfg-btn-primary uppercase w-full">
+            смотреть трансляцию
+          </button>
+        </div>
+        <div class="bg-white p-6 rounded-3xl">
+          <button type="button" class="wfg-btn-primary uppercase w-full">слушать трансляцию</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
