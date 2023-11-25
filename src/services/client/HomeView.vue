8cdda8bbbabcf25cb7f41d7a939e5207899a737b
codemossaka <peyagodson@ymail.com>
2023-11-25 23:01:25 +0300

    home suggestion

--- src/services/client/HomeView.vue
+++ src/services/client/HomeView.vue
@@ -3,14 +3,14 @@
     <div class="relative overflow-hidden h-screen flex items-center justify-center">
       <div>
         <video
-          class="absolute w-full blur-sm object-cover h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          autoplay="autoplay"
-          id="coverVideo"
-          loop="loop"
-          muted="muted"
-          playsinline="playsinline"
+            class="absolute w-full blur-sm object-cover h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            autoplay="autoplay"
+            id="coverVideo"
+            loop="loop"
+            muted="muted"
+            playsinline="playsinline"
         >
-          <source :src="videoSource" type="video/mp4" />
+          <source :src="videoSource" type="video/mp4"/>
         </video>
       </div>
       <div class="z-10 bg-black bg-opacity-20 w-full h-full flex justify-center items-center">
@@ -19,15 +19,29 @@
             <a href="" @click="$router.push({ name: 'HomeView' })" class="">
               <h1 class="uppercase text-4xl text-white lg:text-8xl">Божье Слово</h1>
             </a>
+
+            <div class="grid sm:grid-cols-3 grid-cols-1 gap-4 pt-9 sm:gap-10">
+              <div class="bg-white p-6 rounded-3xl"
+                   v-for="(menu, index) in menus"
+                   :key="index">
+                <router-link
+                    :to="{ name: menu.link }"
+                    type="button"
+                    class="wfg-btn-primary uppercase w-full"
+                >
+                  {{ menu.title }}
+                </router-link>
+              </div>
+            </div>
             <svg
-              class="w-10 h-10 mx-auto mb-3 mt-10 text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 14"
+                class="w-10 h-10 mx-auto mb-3 mt-10 text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 18 14"
             >
               <path
-                d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
+                  d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
               />
             </svg>
             <blockquote>
@@ -41,26 +55,6 @@
               </p>
             </blockquote>
           </figure>
-
-          <div class="grid sm:grid-cols-2 grid-cols-1 gap-4 sm:gap-10">
-            <div class="bg-white p-6 rounded-3xl">
-              <router-link
-                :to="{ name: 'PredicationsView' }"
-                type="button"
-                class="wfg-btn-primary uppercase w-full"
-              >
-                Видео
-              </router-link>
-            </div>
-            <div class="bg-white p-6 rounded-3xl">
-              <router-link
-                :to="{ name: 'StreamingClientDirection' }"
-                type="button"
-                class="wfg-btn-primary uppercase w-full"
-                >Трансляция</router-link
-              >
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -70,7 +64,45 @@
 export default {
   data() {
     return {
-      videoSource: `${import.meta.env.BASE_URL}cover.mp4`
+      videoSource: `${import.meta.env.BASE_URL}cover.mp4`,
+      menus: [
+        {
+          title: "Главная",
+          link: "PredicationsView"
+        },
+        {
+          title: "Архив",
+          link: "PredicationsView"
+        },
+        {
+          title: "Трансляция",
+          link: "StreamingClientDirection"
+        },
+        {
+          title: "Дневник",
+          link: "StreamingClientDirection"
+        },
+        {
+          title: "Свидетельство",
+          link: "StreamingClientDirection"
+        },
+        {
+          title: "Поклонения",
+          link: "PredicationsView"
+        },
+        {
+          title: "Фото",
+          link: "StreamingClientDirection"
+        },
+        {
+          title: "О нас",
+          link: "StreamingClientDirection"
+        },
+        {
+          title: "Контакты",
+          link: "StreamingClientDirection"
+        },
+      ]
     }
   }
 }
