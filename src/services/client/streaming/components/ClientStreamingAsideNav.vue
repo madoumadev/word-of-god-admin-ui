c9d6315970dfdb1882f7437fb7a52918f5e433db
Boumbanda Pama Gondran A <56638172+madoumadev@users.noreply.github.com>
2023-11-25 16:44:09 +0300

    styles

--- src/services/client/streaming/components/ClientStreamingAsideNav.vue
+++ src/services/client/streaming/components/ClientStreamingAsideNav.vue
@@ -34,15 +34,15 @@ export default defineComponent({
   <AsideNavLayout class="bg-white">
     <div class="mt-4 flex flex-col p-6 overflow-hidden space-y-10 divide-y h-full">
       <div class="flex flex-col">
-        <p class="capitalize text-gray-400 text-sm mb-4">БЛИЖАЙШАЯ ТРАНСЛЯЦИЯ</p>
+        <p class="capitalize text-gray-500 text-sm mb-4">БЛИЖАЙШАЯ ТРАНСЛЯЦИЯ</p>
         <div class="inline-flex items-center mb-2 space-x-2">
-          <HeroIcon icon-type="outline" icon-name="CalendarIcon" class="w-4 h-5 text-gray-400" />
+          <HeroIcon icon-type="outline" icon-name="CalendarIcon" class="w-4 h-5 text-gray-500" />
           <p class="text-sm text-black capitalize" v-if="live?.snippet && live?.snippet?.localized">
-            {{ live?.snippet?.localized?.title }}}
+            {{ live?.snippet?.localized?.title }}
           </p>
         </div>
         <div class="inline-flex items-center space-x-2">
-          <HeroIcon icon-type="outline" icon-name="ClockIcon" class="w-4 h-5 text-gray-400" />
+          <HeroIcon icon-type="outline" icon-name="ClockIcon" class="w-4 h-5 text-gray-500" />
           <p class="text-sm capitalize text-black">
             В {{ timestampToLocalTime(live?.snippet?.publishedAt?.value)?.toString() }}
           </p>
@@ -50,7 +50,7 @@ export default defineComponent({
       </div>
 
       <div class="flex flex-col space-y-2 pt-4">
-        <p class="uppercase text-gray-400 text-sm mb-4">СЛУЖЕНИЯ</p>
+        <p class="uppercase text-gray-500 text-sm mb-4">СЛУЖЕНИЯ</p>
         <ul class="list-disc pl-4">
           <li class="text-sm text-black capitalize mb-2">ВОСКРЕСЕНЬЕ - 12:00,</li>
           <li class="text-sm text-black capitalize">СРЕДА - 19:00</li>
@@ -58,7 +58,7 @@ export default defineComponent({
       </div>
 
       <div class="flex flex-col space-y-2 pt-4">
-        <p class="uppercase text-gray-400 text-sm mb-4">КОНТАКТЫ</p>
+        <p class="uppercase text-gray-500 text-sm mb-4">КОНТАКТЫ</p>
         <div class="">
           <img src="../../../../assets/email.png" class="w-34 scale-110" alt="email" />
         </div>
