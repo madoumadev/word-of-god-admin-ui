a0a6a0fcc6242a958b9a73fc4fa05a6e098556c9
Boumbanda Pama Gondran A <56638172+madoumadev@users.noreply.github.com>
2023-12-05 23:48:43 +0300

    home page

--- src/services/client/HomeView.vue
+++ src/services/client/HomeView.vue
@@ -3,55 +3,60 @@
     <div class="relative overflow-hidden h-screen flex items-center justify-center">
       <div>
         <video
-            class="absolute w-full blur-sm object-cover h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            autoplay="autoplay"
-            id="coverVideo"
-            loop="loop"
-            muted="muted"
-            playsinline="playsinline"
+          class="absolute w-full blur-sm object-cover h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+          autoplay="autoplay"
+          id="coverVideo"
+          loop="loop"
+          muted="muted"
+          playsinline="playsinline"
         >
-          <source :src="videoSource" type="video/mp4"/>
+          <source :src="videoSource" type="video/mp4" />
         </video>
       </div>
       <div class="z-10 bg-black bg-opacity-20 w-full h-full flex justify-center items-center">
-        <div class="max-w-screen-md mx-auto px-10 sm:px-0">
+        <div class="lg:max-w-screen-md max-w-screen-sm mx-auto px-10 sm:px-0">
           <figure class="my-10 text-center">
-            <a href="" @click="$router.push({ name: 'HomeView' })" class="">
-              <h1 class="uppercase text-4xl text-white lg:text-8xl">Божье Слово</h1>
+            <a
+              href=""
+              @click="$router.push({ name: 'HomeView' })"
+              class="flex flex-col items-center justify-center"
+            >
+              <span class="w-16 lg:w-32"
+                ><img
+                  src="https://wordofgod.ru/design/wog/img/o.png"
+                  class="w-full h-full object-cover"
+                  alt="word of God"
+              /></span>
+              <h1 class="text-white logo">Божье Слово Верно</h1>
             </a>
 
-            <div class="grid sm:grid-cols-3 grid-cols-1 gap-4 pt-9 sm:gap-10">
-              <div class="bg-white p-6 rounded-3xl"
-                   v-for="(menu, index) in menus"
-                   :key="index">
-                <router-link
-                    :to="{ name: menu.link }"
-                    type="button"
-                    class="wfg-btn-primary uppercase w-full"
-                >
+            <div class="grid lg:grid-cols-3 grid-cols-2 gap-4 pt-9 lg:gap-6">
+              <router-link
+                :class="[index === menus.length - 1 ? 'lg:col-span-1 col-span-2' : '']"
+                class="p-4 lg:p-6 border-l-2 hover:bg-white backdrop-blur-xl hover:border-b-primary shadow-md rounded-md bg-black group bg-opacity-30 hover:bg-opacity-20 transition-all duration-200"
+                v-for="(menu, index) in menus"
+                :key="index"
+                :to="{ name: menu.link }"
+              >
+                <span class="wfg-btn-primary uppercase text-white w-full group-hover:font-bold">
                   {{ menu.title }}
-                </router-link>
-              </div>
+                </span>
+              </router-link>
             </div>
             <svg
-                class="w-10 h-10 mx-auto mb-3 mt-10 text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 14"
+              class="w-6 h-6 lg:w-10 lg:h-10 mx-auto mb-3 mt-6 lg:mt-10 text-white text-opacity-40"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 14"
             >
               <path
-                  d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
+                d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
               />
             </svg>
             <blockquote>
-              <p class="text-xs lg:text-lg italic text-white">
-                "Когда я думаю об Африке, Индии и о всём мире, миллионы язычников кричат и вопиют о
-                милости, и кто пойдёт? Не брошюры им раздавать, но принести им Иисуса Христа. Кто-то
-                в Его Присутствии, как Моисей, который мог бы отправиться туда и показать им
-                истинное избавление. Не заставлять их присоединяться к церкви или пожимать руку и
-                иметь вероучение, но донести их душе избавление; какой-нибудь хороший благочестивый
-                человек."
+              <p class="text-xs lg:text-lg italic text-white bg-black bg-opacity-10 shadow-md">
+                {{ news.isActive ? news.content : staticInfo.content }}
               </p>
             </blockquote>
           </figure>
@@ -65,43 +70,61 @@ export default {
   data() {
     return {
       videoSource: `${import.meta.env.BASE_URL}cover.mp4`,
+      staticInfo: {
+        id: 1,
+        content:
+          'Когда я думаю об Африке, Индии и о всём мире, миллионы язычников кричат и вопиют о\n' +
+          '                милости, и кто пойдёт? Не брошюры им раздавать, но принести им Иисуса Христа. Кто-то\n' +
+          '                в Его Присутствии, как Моисей, который мог бы отправиться туда и показать им\n' +
+          '                истинное избавление. Не заставлять их присоединяться к церкви или пожимать руку и\n' +
+          '                иметь вероучение, но донести их душе избавление; какой-нибудь хороший благочестивый\n' +
+          '                человек.'
+      },
+      news: {
+        id: 1,
+        content:
+          'С 4 по 6 января 2024г. в скинии планируется проведение молодежных служений.\n' +
+          'Начало служений в 12:00, обед, затем общение.\n' +
+          'Желающие посетить служения и имеющие необходимость в размещении, могут отправить запрос на info@wordofgod.ru',
+        isActive: true
+      },
       menus: [
         {
-          title: "Главная",
-          link: "PredicationsView"
+          title: 'Главная',
+          link: 'PredicationsView'
         },
         {
-          title: "Архив",
-          link: "PredicationsView"
+          title: 'Архив',
+          link: 'PredicationsView'
         },
         {
-          title: "Трансляция",
-          link: "StreamingClientDirection"
+          title: 'Трансляция',
+          link: 'StreamingClientDirection'
         },
         {
-          title: "Дневник",
-          link: "StreamingClientDirection"
+          title: 'Дневник',
+          link: 'StreamingClientDirection'
         },
         {
-          title: "Свидетельство",
-          link: "StreamingClientDirection"
+          title: 'Свидетельство',
+          link: 'StreamingClientDirection'
         },
         {
-          title: "Поклонения",
-          link: "PredicationsView"
+          title: 'Поклонения',
+          link: 'PredicationsView'
         },
         {
-          title: "Фото",
-          link: "StreamingClientDirection"
+          title: 'Фото',
+          link: 'StreamingClientDirection'
         },
         {
-          title: "О нас",
-          link: "StreamingClientDirection"
+          title: 'О нас',
+          link: 'StreamingClientDirection'
         },
         {
-          title: "Контакты",
-          link: "StreamingClientDirection"
-        },
+          title: 'Контакты',
+          link: 'StreamingClientDirection'
+        }
       ]
     }
   }
